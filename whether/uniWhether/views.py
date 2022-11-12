1252792b5e6f21e5d357db00376e27565e52f343
joshua urasa <urasajoshuag@gmail.com>
2022-11-13 02:10:20 +0300

    second  commit

--- whether/uniWhether/views.py
+++ whether/uniWhether/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
+import urllib
+import json
 
 def index(request):
-    return render(request,'index.html',{})
+    if request.method == "POST":
+        city=request.POST.get('city')
+        api_url=urllib.request.urlopen(f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=63c41ac247956b75d1b2b08196a5d2c5').read()
+        api_2=json.loads(api_url)
+        print(api_2)
+
+        data = {
+            'city':city,
+            'whether_description':api_2['weather'][0]['description'],
+            'whether_temperature':api_2['main']['temp'],
+            'whether_humidity':api_2['main']['humidity'],
+            'whether_pressure':api_2['main']['pressure'],
+        }
+    else:
+        data = {
+            'city':None,
+            'whether_description':None,
+            'whether_temperature':None,
+            'whether_humidity':None,
+            'whether_pressure':None,
+        }
+
+    return render(request,'index.html',{'data':data})
--- whether/whether/settings.py
+++ whether/whether/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'uniWhether',
     'django_bootstrap5',
+    'octicons',
 ]
 
 MIDDLEWARE = [
